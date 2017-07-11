b17182877fdc9a3f39c9cbd100c2b2030eb048e7
Lazyman <771623548@qq.com>
2017-07-11 11:51:03 +0800

    Update register_face.php

--- register_face.php
+++ register_face.php
@@ -37,9 +37,9 @@ $(function(){
 require_once './tools/baidu_face/AipFace.php';
 
 // 定义常量
-const APP_ID = '9872531';
-const API_KEY = 'GGeypgX3jQ77fZ4cvWzwPCGw';
-const SECRET_KEY = 'OQYEnon5XMM4jx7MalTo7t7qesBAOdfg';
+const APP_ID = '';
+const API_KEY = '';
+const SECRET_KEY = '';
 
 // 初始化
 $aipFace = new AipFace(APP_ID, API_KEY, SECRET_KEY);
